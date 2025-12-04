5f8a4a4c1d001d853f978c45a5e815b788409f01
Alan Sebastian <sebastianalan360@gmail.com>
2025-12-04 13:39:00 +1030

    Update Header.h

--- Header.h
+++ Header.h
@@ -2,9 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
-#include <conio.h>
 #include <stdlib.h>
-#define GEN_SIZE 89
 typedef struct {
 	char* name;
 	float components[3];
@@ -15,5 +13,6 @@ void vector_unit(float Vec[], Vector* vector, int* VecCount);
 void vector_projection(float Vec1[], float onto_Vec2[]);
 void vector_cross_product(float vec1[], float vec2[], float result[]);
 float angle_between_vectors(float vec1[], float vec2[]);
+
 
 
